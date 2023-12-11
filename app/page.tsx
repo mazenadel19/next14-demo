0bd6173c6f81ac9c1f3a1a71c38865db69433afc
Mazen <mazenadel19@gmail.com>
2023-12-11 16:35:32 +0200

    feat: :white_check_mark: add questions and answers
    use server actions to reveal correct answer

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,35 @@
+import { Suspense } from 'react';
 import Link from "next/link";
+import { supabase } from '@/supabase';
+
+async function Quizzes() {
+  const { data, error } = await supabase.from('questions').select('*');
+
+  if (error) {
+    console.error('Error:', error);
+    return (
+      <p className="text-red-500 text-lg font-semibold mt-4">Something went wrong...</p>
+    );
+  } else {
+    return (
+      <ul className='grid grid-cols-3 w-100'>
+        {data.map((quiz) => (
+          <li key={quiz.id} className='p-4 justify-center flex text-sky-500 hover:text-sky-700 hover:underline'>
+            <Link href={`/quiz/${quiz.id}`}>{quiz.question_text}</Link>
+          </li>
+        ))}
+      </ul>
+    )
+  }
+}
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>All Quizzes</h1>
-      <ul>
-        <li>
-          <Link href="/quiz/1">Quiz 1</Link>
-        </li>
-        <li>
-          <Link href="/quiz/2">Quiz 2</Link>
-        </li>
-        <li>
-          <Link href="/quiz/3">Quiz 3</Link>
-        </li>
-        <li>
-          <Link href="/quiz/4">Quiz 4</Link>
-        </li>
-      </ul>
+    <main className="flex min-h-screen flex-col items-center justify-evenly p-24">
+      <h1 className="text-3xl font-bold flex justify-center">All Quizzes</h1>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Quizzes />
+      </Suspense>
     </main>
   )
 }
--- app/quiz/[id]/page.tsx
+++ app/quiz/[id]/page.tsx
@@ -1,20 +1,81 @@
-import Link from "next/link"
+import Link from "next/link";
+import { redirect } from "next/navigation";
+import { supabase } from "@/supabase";
+
+async function Quiz({ id, show }: { id: string, show?: boolean }) {
+    const { data, error } = await supabase
+        .from('questions')
+        .select(`
+    id,
+    title,
+    description,
+    question_text,
+    answers (
+      id,
+      quiz_id,
+      answer_text,
+      is_correct
+    )
+  `).eq('id', id);
+
+    if (error) {
+        console.error('Error:', error);
+        return (
+            <p className="text-red-500 text-lg font-semibold mt-4">Something went wrong...</p>
+        );
+    } else {
+        const question = data[0]
+        return (
+            <div className="bg-gray-100 p-4 rounded-lg shadow-md">
+                <p className="italic text-gray-600 mb-4">{question.description}</p>
+                <ul className="list-disc pl-6">
+                    {question.answers.map((answer) => {
+                        const isCorrect = answer.is_correct ? 'text-green-700' : 'text-red-700'
+                        const revealAnswers = show ? isCorrect : ""
+                        return (
+                            <li key={answer.id} className={`mb-1 ${revealAnswers}`}>
+                                {answer.answer_text}
+                            </li>
+                        )
+                    }
+                    )}
+                </ul>
+            </div>
+        );
+    }
+}
+
 
 interface Props {
     params: {
         id: string
+    },
+    searchParams: {
+        show?: boolean
     }
 }
 
-function QuizPage({ params }: Props) {
+function QuizPage({ params, searchParams }: Props) {
     return (
         <section>
             <ul>
                 <li>
-                    <Link href="/">back</Link>
+                    <Link href="/" className='p-4 text-sky-500 hover:text-sky-700 hover:underline'>
+                        🔙 Go back
+                    </Link>
                 </li>
             </ul>
-            <h1>Quiz {params.id}</h1>
+            <br />
+            <Quiz id={params.id} show={searchParams?.show} />
+            <br />
+            <form action={async () => {
+                'use server';
+                redirect(`/quiz/${params.id}?show=true`);
+            }}>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    Reveal Correct Answer
+                </button>
+            </form>
         </section>
     )
 }
